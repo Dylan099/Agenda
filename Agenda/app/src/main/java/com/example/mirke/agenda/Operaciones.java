f0151ff146b52aaeec870007739aec05e9990fc2
ZephonTV <dylan.mariscal.salas@gmail.com>
2019-08-22 15:09:15 -0400

    Proyecto de Agenda con Persistencia de Datos

---
+++ Agenda/app/src/main/java/com/example/mirke/agenda/Operaciones.java
@@ -0,0 +1,8 @@
+package com.example.mirke.agenda;
+
+public interface Operaciones {
+
+    public void agregar(Contacto contacto);
+    public void quitar(Contacto contacto);
+    public void modificar(Contacto contacto,int position);
+}
